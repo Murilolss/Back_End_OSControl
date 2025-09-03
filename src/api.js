967d1f881cdaacdb49952bdf565013497d5d584b
muriloap <muprado222@gmail.com>
2025-09-03 17:14:25 -0300

    API

--- src/api.js
+++ src/api.js
@@ -2,5 +2,37 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 
-import serviceRoutes from './routers/service'
+import clientRoutes from './routers/client'
 import orderRoutes from './routers/order'
+import productRoutes from './routers/product'
+import serviceRoutes from './routers/service'
+import shopRoutes from './routers/shop'
+import signatureRoutes from './routers/signature'
+import userRoutes from './routers/user'
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/clients', clientRoutes)
+app.use('/orders', orderRoutes)
+app.use('/products', productRoutes)
+app.use('/services', serviceRoutes)
+app.use('/shops', shopRoutes)
+app.use('/signatures', signatureRoutes)
+app.use('/users', userRoutes)
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    if (err.code === 'P2002'){
+        return res.status(409).json({
+            error: 'Registro duplicado (unique)'
+        });
+    }
+    if (err.code === 'P2025'){
+        return res.status(404).json({
+            error: 'Registro não encontrado'
+        });
+    }
+    res.status(500).json({error: 'Erro interno'});
+});
