4710ebb3532afeb62e3d0561c658e0fb543f3a2c
muriloap <muprado222@gmail.com>
2025-09-30 16:28:42 -0300

    correção de erro

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,8 +1,8 @@
 import jwt from 'jsonwebtoken'
 
 export function verificaToken(req, res, next){
-    const auft = req.headers.authorization || "";
-    const token = auft.startsWith("Bearer ") ? auft.slice(7) : null;
+    const auth = req.headers.authorization || "";
+    const token = auft.startsWith("Bearer ") ? auth.slice(7) : null;
 
     if (!token){
         res.status(401).json({error: "Token Não enviado"})
